e365b39d570d168c9b40b80ff0deabf31338ee32
akiyo yokota <ayokota@ucrwpa1-2-134-209.bulk.ucr.edu>
2015-10-16 14:15:50 -0700

    added simple track, simple car, but does not drive correctly

---
+++ UCRacer/Assets/Scripts/CarDriving.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarDriving : MonoBehaviour {
+	
+	float speed = 20f;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.W))
+		{
+			GetComponent<Rigidbody>().AddRelativeForce (Vector3.forward * Input.GetAxis ("Vertical") * (speed));
+			
+		}
+		if (Input.GetKey (KeyCode.S))
+		{
+			GetComponent<Rigidbody>().AddRelativeForce ( Vector3.back * Input.GetAxis ("Vertical") * (-1) * (speed));
+		}
+		if (Input.GetKey (KeyCode.A))
+		{
+			transform.Rotate (new Vector3 (0, -1, 0) );
+		}
+		if (Input.GetKey (KeyCode.D))
+		{
+			transform.Rotate (new Vector3 (0, 1, 0) );;
+		}
+	}
+}
