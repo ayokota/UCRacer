d2760e8d94a1fea0c08dc23f7518b5321cc7196c
vpang002 <vpang002@ucr.edu>
2015-10-18 17:54:57 -0700

    Started implementing wheel colliders
    still needs lots of bug fixes..

---
+++ UCRacer/Assets/Scripts/VincentCarDriving.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VincentCarDriving : MonoBehaviour {
+	
+	public float accel;
+	public float steer;
+	public WheelCollider FrontRight;
+	public WheelCollider FrontLeft;
+	public WheelCollider BackRight;
+	public WheelCollider BackLeft;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float vert = Input.GetAxis ("Vertical") * accel;
+		float hori = Input.GetAxis ("Horizontal") * steer;
+
+		BackRight.motorTorque = vert;
+		BackLeft.motorTorque = vert;
+
+		FrontRight.steerAngle = hori;
+		FrontLeft.steerAngle = hori;
+	}
+}
