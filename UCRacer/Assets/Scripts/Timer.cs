c5f812a98c93c61ea4e8b019f5f6cb3da62c410b
vpang002 <vpang002@ucr.edu>
2015-11-13 12:31:42 -0800

    Added second track. Keeps track of player's best time

--- UCRacer/Assets/Scripts/Timer.cs
+++ UCRacer/Assets/Scripts/Timer.cs
@@ -5,11 +5,13 @@ public class Timer : MonoBehaviour {
 
 	private float elapsedTime;
 	private bool started;
+	private float bestTime;
 
 	// Use this for initialization
 	void Start () {
 		elapsedTime = 0;
 		started = false;
+		bestTime = 0;
 	}
 	
 	// Update is called once per frame
@@ -20,17 +22,30 @@ public class Timer : MonoBehaviour {
 	}
 
 	void OnTriggerEnter(Collider other) {
-		elapsedTime = 0;
-		started = true;
+		if (other.tag == "Player") {
+			if (bestTime == 0 || elapsedTime < bestTime) {
+				bestTime = elapsedTime;
+			}
+			elapsedTime = 0;
+			started = true;
+		}
 	}
 
 	void OnGUI() {
-		int minutes = Mathf.FloorToInt (elapsedTime / 60F);
-		int seconds = Mathf.FloorToInt (elapsedTime - minutes * 60);
-		string niceTime = string.Format ("{00:00}:{1:00}", minutes, seconds);
+		int elapsedMinutes = Mathf.FloorToInt (elapsedTime / 60F);
+		int elapsedSeconds = Mathf.FloorToInt (elapsedTime - elapsedMinutes * 60);
+		string niceTime = string.Format ("{00:00}:{1:00}", elapsedMinutes, elapsedSeconds);
 		GUI.contentColor = Color.black;
 		GUIStyle style = new GUIStyle();
 		style.fontSize = 40;
 		GUI.Label ( new Rect (Screen.width/2 - 50, 20, 200, 200), niceTime, style);
+
+		int bestMinutes = Mathf.FloorToInt (bestTime / 60F);
+		int bestSeconds = Mathf.FloorToInt (bestTime - bestMinutes * 60);
+		niceTime = string.Format ("{00:00}:{1:00}", bestMinutes, bestSeconds);
+		GUI.contentColor = Color.black;
+		style = new GUIStyle();
+		style.fontSize = 20;
+		GUI.Label ( new Rect (Screen.width/2 - 50, 60, 200, 200), "best: " + niceTime, style);
 	}
 }
