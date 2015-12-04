67b1b7c41828e063aa25275138afc0707651ef68
vpang002 <vpang002@ucr.edu>
2015-12-03 18:51:22 -0800

    Placing and best lap time are displayed at end of race

---
+++ UCRacer/Assets/Scripts/EndSceneScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class EndSceneScript : MonoBehaviour {
+
+	public int Mins;
+	public int Secs;
+
+	// Use this for initialization
+	void Start () {
+		Storage stor = GameObject.FindGameObjectWithTag("Storage").GetComponent<Storage>();
+		Text t = GameObject.Find ("BestTime").GetComponent<Text>();
+
+		Mins = Mathf.FloorToInt (stor.bestTime / 60F);
+		Secs = Mathf.FloorToInt (stor.bestTime - Mins * 60F);
+		string niceTime = string.Format ("{00:00}:{1:00}", Mins, Secs);
+
+		t.text = "Your Best Time: " + niceTime;
+
+		t = GameObject.Find ("Placing").GetComponent<Text> ();
+		if (stor.beatAI == true) {
+			t.text = "You Came In 1st Place!";
+		} 
+		else {
+			t.text = "You Lost. What a Loser.";
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void loadscene()
+	{
+		Application.LoadLevel("menu"); // replace with post-login menu scene name
+	}
+}
